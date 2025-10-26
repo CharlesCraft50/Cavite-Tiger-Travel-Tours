995002ded1faf8e4b751b849fae59e2bc404eb3e
CharlesCraft50 <charlescraft50yt@gmail.com>
2025-10-27 00:09:21 +0800

    s

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\NotificationApiController;
+use App\Http\Controllers\Api\PackageApiController;
+use App\Http\Controllers\Api\VanApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\VanApiController;
-use App\Http\Controllers\Api\PackageApiController;
-use App\Http\Controllers\Api\NotificationApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/van/{vanId}/availability', [VanApiController::class, 'availability']);
 Route::get('/packages/latest', [PackageApiController::class, 'getLatestPackages']);
-Route::get('/packages', [PackageApiController::class, 'index'])->name('packages.index');
+Route::get('/packages', [PackageApiController::class, 'index'])->name('api.packages.index');
 
 Route::get('/notifications/{userId}', [NotificationApiController::class, 'index']);
 Route::get('/notifications/{userId}/read', [NotificationApiController::class, 'markAllAsRead']);
