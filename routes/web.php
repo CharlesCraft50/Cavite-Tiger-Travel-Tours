c206ae5d8a261366b75d377298afed3caab0e561
CharlesCraft50 <charlescraft50yt@gmail.com>
2025-11-04 18:05:33 +0800

    Verified

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,26 @@ use App\Http\Controllers\PreferredVanController;
 use App\Http\Controllers\VanCategoryController;
 use App\Http\Controllers\WishlistController;
 use App\Models\TourPackage;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::get('/email/verify', function () {
+    return Inertia\Inertia::render('pages/auth/verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::get('/', function () {
     $packages = TourPackage::whereIn('id', [1, 2, 3, 4, 5, 6])->get();
